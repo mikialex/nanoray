5f51ba01635930a18cd86733c00e634e461fbf21
mikialex <18516340862@163.com>
2018-01-15 22:43:20 +0800

    intial commit

---
+++ webpack.config.js
@@ -0,0 +1,72 @@
+var path = require('path');
+var webpack = require('webpack');
+var HtmlwebpackPlugin = require('html-webpack-plugin');
+
+var ROOT_PATH = path.resolve(__dirname);
+var APP_PATH = path.resolve(ROOT_PATH, 'src');
+var BUILD_PATH = path.resolve(ROOT_PATH, 'dist');
+var TEM_PATH = path.resolve(ROOT_PATH, '');
+
+function resolve(dir) {
+  return path.join(__dirname, '..', dir)
+}
+
+module.exports = {
+  entry: {
+    app: path.resolve(APP_PATH, 'index.ts'),
+    // vendors: []
+  },
+  output: {
+    path: BUILD_PATH,
+    filename: '[name].js'
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+    alias: {
+      // '@': resolve('src'),
+    }
+  },
+  // // enable dev source map
+  devtool: 'eval-source-map',
+  // // enable dev server
+  devServer: {
+    hot: true,
+    // contentBase: path.resolve(__dirname, 'dist'),
+    // publicPath: '/'
+  },
+  module: {
+    rules: [{
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          // name: utils.assetsPath('img/[name].[hash:7].[ext]')
+        }
+      },
+      {
+        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          // name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
+        }
+      },
+    ]
+  },
+
+  plugins: [
+    new HtmlwebpackPlugin({
+      title: 'vox world',
+      template: path.resolve(__dirname, 'index.html'),
+      filename: 'index.html',
+      chunks: ['app', 'vendors'],
+      inject: 'body'
+    }),
+    new webpack.HotModuleReplacementPlugin() // Enable HMR
+  ]
+};
